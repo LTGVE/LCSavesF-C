be3156a61548fcebfe19d7e729e2a6b2a9954ccb
LT_GVE <1638666724@qq.com>
2024-12-21 17:56:32 +0800

    添加项目文件。

---
+++ LethalCompanySavesFinder/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace LethalCompanySavesFinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.Saves = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 9F);
+            this.button1.Location = new System.Drawing.Point(136, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 74);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "复制";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Saves
+            // 
+            this.Saves.Font = new System.Drawing.Font("微软雅黑", 9F);
+            this.Saves.FormattingEnabled = true;
+            this.Saves.ItemHeight = 17;
+            this.Saves.Location = new System.Drawing.Point(2, 2);
+            this.Saves.Name = "Saves";
+            this.Saves.Size = new System.Drawing.Size(128, 157);
+            this.Saves.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 9F);
+            this.label1.Location = new System.Drawing.Point(-1, 162);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Dev LT_GVE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 7.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(81, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "把存档文件拖进来可以添加/替换存档";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 9F);
+            this.button2.Location = new System.Drawing.Point(136, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 77);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "删除";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.ClientSize = new System.Drawing.Size(249, 180);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Saves);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LC存档复制与删除";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox Saves;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ LethalCompanySavesFinder/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Windows.Forms;
+
+namespace LethalCompanySavesFinder
+{
+    public partial class Form1 : Form
+    {
+        public static string SavePath = $"C:/Users/{Environment.UserName}/AppData/LocalLow/ZeekerssRBLX/Lethal Company";
+        public List<string> saves=new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            refreshFile();
+            this.DragEnter += new DragEventHandler(Form_DragEnter);
+            this.DragDrop += new DragEventHandler(FileDrag);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Saves.SelectedItem == null)
+            {
+                MessageBox.Show("请先选择存档", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            var selectedFile = SavePath + $"/{Saves.SelectedItem}";
+            Clipboard.SetFileDropList(new StringCollection() { selectedFile });
+            MessageBox.Show("已复制到剪贴板","提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void Form_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+            public void FileDrag(Object sender, DragEventArgs e) {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files.Length>1) {
+                MessageBox.Show("请勿拖动多个文件(懒得写多文件拖动)","警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            string file = files[0];
+            if (Saves.SelectedItem==null) {
+                var addfile = SavePath+"/LCSaveFile"+(Saves.Items.Count+1);
+                MessageBox.Show("存档已添加", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                File.WriteAllBytes(addfile, File.ReadAllBytes(file));
+                refreshFile();
+                return;
+            }
+            var selectedFile = SavePath + $"/{Saves.SelectedItem}";
+            File.Delete(selectedFile);
+            File.WriteAllBytes(selectedFile, File.ReadAllBytes(file));
+            MessageBox.Show("存档已替换","提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            refreshFile();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Saves.SelectedItem == null)
+            {
+                MessageBox.Show("请先选择存档", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            var selectedFile = SavePath + $"/{Saves.SelectedItem}";
+            File.Delete(selectedFile);
+            MessageBox.Show("存档已删除", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            refreshFile();
+        }
+        public void refreshFile() {
+            saves.Clear();
+            Saves.Items.Clear();
+            DirectoryInfo folder = new DirectoryInfo(SavePath);
+            foreach (FileInfo file in folder.GetFiles("*", SearchOption.TopDirectoryOnly))
+            {
+                if (file.Extension == "" && file.Name.Contains("SaveFile"))
+                {
+                    saves.Add(file.Name);
+                }
+            }
+            Saves.Items.AddRange(saves.ToArray());
+        }
+    }
+}
